d68dd11497dfbaf7ab318a82054d406701bb3ded
chandanyadav2004 <demo8788151949@gmail.com>
2025-08-02 13:46:11 +0530

    login page and sign up page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,18 @@ import ChatBot from './pages/ChatBot'
 import Connections from './pages/Connections'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
+import Layout from './pages/Layout'
 import CreatePost from './pages/CreatePost'
+import { useUser } from "@clerk/clerk-react"
 
 const App = () => {
+
+  const { user } = useUser()
+
   return (
     <>
         <Routes>
-          <Route path='/' element={<Login /> }>
+          <Route path='/' element={ !user ? <Login /> : <Layout/> }>
             <Route index  element={ <Feed />} />
             <Route path='messages'  element={ <Messages />} />
             <Route path='messages/:userId'  element={ <ChatBot />} />
