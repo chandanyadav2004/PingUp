44974f040dcd7d24c38fd5e8681244554d776673
chandanyadav2004 <demo8788151949@gmail.com>
2025-08-08 21:30:56 +0530

    ProfileModal completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import Loading from "../components/Loading";
 import UserProfileInfo from "../components/UserProfileInfo";
 import PostCard from "../components/PostCard";
 import moment from "moment";
+import ProfileModal from "../components/ProfileModal";
 
 const Profile = () => {
   const { profileId } = useParams();
@@ -103,7 +104,7 @@ const Profile = () => {
       {/* Edit Profile Modal */}
       {
         showEdit && 
-          <p> Show profile edit</p>
+          <ProfileModal setShowEdit={setShowEdit} />
         
       }
     </div>
