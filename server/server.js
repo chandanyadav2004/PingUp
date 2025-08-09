10412d04863732db7d6d7bc0894d4552aaa1879c
chandanyadav2004 <demo8788151949@gmail.com>
2025-08-09 11:54:29 +0530

    port change

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
