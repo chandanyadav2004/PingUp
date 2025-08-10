f203f85b0c53725d1b421a13cb425cd252c28b12
chandanyadav2004 <demo8788151949@gmail.com>
2025-08-10 18:16:37 +0530

    message notification fn

--- server/Inngest/index.js
+++ server/Inngest/index.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import Connection from "../models/Connection.js";
 import sendEmail from "../configs/nodeMailer.js";
 import Story from "../models/Story.js";
+import Message from "../models/Message.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -143,11 +144,55 @@ const deleteStory = inngest.createFunction(
   }
 )
 
+const sendNotificationOfUnseenMessages = inngest.createFunction(
+  {id: "send-unseen-messages-notification"},
+  {cron: "TZ=America/New_York 0 9 * * * " }, // Every day 9 AM
+  async ({step}) => {
+    const messages = await Message.find({seen: false}).populate('to_user_id');
+    const unseenCount = {}
+    messages.map(message => {
+      unseenCount[message.to_user_id._id] = (unseenCount[message.to_user_id._id] || 0)
+      + 1 ;
+    })
+    for(const userId in unseenCount){
+      const user = await User.findById(userId);
+
+      const subject = ` You have ${unseenCount[userId]} unseen messages`;
+      const body = `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+          <h2>Hi ${user.full_name},</h2>
+          <p>You have ${unseenCount[userId] } unseen messages</p>
+          <p>
+            Click <a href="${process.env.FRONTEND_URL}/messages" style="color: #10b981;">here</a> to view them
+          </p>
+          <br/>
+          <p>Thanks,<br/>PingUp – Stay Connected</p>
+        </div>
+        `;
+
+        await sendEmail({
+          to: user.email,
+          subject,
+          body
+        })
+
+      
+    }
+    return {message: "Notification sent"}
+  }
+)
+
+
+
+
+
+
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
   sendNewConnectionRequestReminder,
-  deleteStory
+  deleteStory,
+  sendNotificationOfUnseenMessages
 ];
