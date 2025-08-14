a0d008bd924ddfaea6a405a0731bfc383d211251
chandanyadav2004 <demo8788151949@gmail.com>
2025-08-14 20:58:15 +0530

    App.jsx changes completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useRef } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
 import Messages from "./pages/Messages";
@@ -10,19 +10,57 @@ import Profile from "./pages/Profile";
 import Layout from "./pages/Layout";
 import CreatePost from "./pages/CreatePost";
 import { useUser, useAuth } from "@clerk/clerk-react";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "./features/user/userSlice";
+import { fetchConnections } from "./features/connections/connectionsSlice";
+import { addMessage } from "./features/messages/messagesSlice";
+import Notifications from "./components/Notifications";
 
 const App = () => {
+  const { pathname} = useLocation()
   const { user } = useUser();
   const { getToken } = useAuth();
+  const dispatch = useDispatch();
+  const pathnameRef = useRef(pathname)
+
+  useEffect(() => {
+  const fetchData = async () => {
+    if (user) {
+      let token = await getToken();
+      console.log(token)
+      dispatch(fetchUser(token));
+      dispatch(fetchConnections(token))
+    }
+  }
+  fetchData();
+}, [user, getToken, dispatch]);
+
+useEffect(()=>{
+  pathnameRef.current = pathname
+},[pathname])
 
   useEffect(()=>{
     if(user){
-      getToken().then((token)=>console.log(token)
-      )
+      const eventSource = new EventSource(import.meta.env.VITE_BASEURL + '/api/message/'+ user.id)
+
+      eventSource.onmessage = (event) =>{
+        const message = JSON.parse(event.data)
+        if(pathnameRef.current === ('/messages/'+ message.from_user_id._id)){
+          dispatch(addMessage(message))
+        }else{
+            toast.custom((t) => ( 
+              <Notifications t={t} message={message} />
+            ), { position: 'bottom-right'})
+        }
+      }
+      return () => {
+        eventSource.close()
+      }
     }
-  },[user])
+  },[user, dispatch])
+
 
   return (
     <>
