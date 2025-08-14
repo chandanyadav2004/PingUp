f3ceff1a4f1c71a3903ab88229558a5ba866fde3
chandanyadav2004 <demo8788151949@gmail.com>
2025-08-14 21:00:55 +0530

    chatbot.jsx changes completed

--- client/src/pages/ChatBot.jsx
+++ client/src/pages/ChatBot.jsx
@@ -1,19 +1,85 @@
 import { useEffect, useRef, useState } from "react";
 import { dummyMessagesData, dummyUserData } from "../assets/assets";
 import { ImageIcon, SendHorizonal } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
+import api from "../api/axios";
+import { addMessage, fetchMessages, resetMessages } from "../features/messages/messagesSlice";
+import toast from "react-hot-toast";
 
 const ChatBot = () => {
-  const messsages = dummyMessagesData;
+  const {messages} = useSelector((state) => state.messages);
+  const { userId} = useParams()
+  const { getToken } = useAuth()
+  const dispatch = useDispatch()
   const [text, setText] = useState("");
   const [image, setImage] = useState(null);
-  const [user, setUser] = useState(dummyUserData);
+  const [user, setUser] = useState(null);
 
   const messagesEndRef = useRef(null);
 
-  const sendMessage = async () => {};
+  const connections = useSelector((state) => state.connections.connections)
+
+  const fetchUserMessages = async () => {
+    try {
+      const token = await  getToken();
+      dispatch(fetchMessages({token, userId}))
+    } catch (error) {
+      toast.error(error.message)
+      
+    }
+  }
+
+  const sendMessage = async () => {
+    try {
+      if(!text && !image) return 
+
+      const token = await getToken();
+      const formData = new FormData();
+      formData.append('to_user_id', userId);
+      formData.append('text',text)
+      image && formData.append('image',image)
+
+      const { data } = await api.post('/api/message/send', formData,{
+        headers: { Authorization: `Bearer ${token}`}
+      })
+      
+      if(data.success){
+        setText('')
+        setImage(null)
+        dispatch(addMessage(data.message))
+      }else {
+        throw new Error(data.message)
+      }
+
+
+    } catch (error) {
+      toast.error(error.message)
+      
+    }
+  };
+
+  useEffect(()=> {
+    fetchUserMessages()
+
+    return() => {{
+      dispatch(resetMessages())
+    }}
+    
+  } , [userId])
+
+  useEffect(()=> {
+    if(connections.length > 0){
+      const user = connections.find(connection => connection._id  === userId)
+      setUser(user)
+    }
+  } ,[connections,userId])
+
+
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messsages]);
+  }, [messages]);
 
   return (
     user && (
@@ -32,8 +98,8 @@ const ChatBot = () => {
 
         <div className=" p-5 md:px-10 h-full overflow-y-scroll">
           <div className=" space-y-4 max-w-4xl mx-auto">
-            {messsages
-              .toSorted((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+            {[...messages]
+              .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
               .map((message, index) => (
                 <div
                   className={` flex flex-col ${
