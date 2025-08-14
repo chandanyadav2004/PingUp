56ef161f3abc0bde6364dae62200203867f785d2
chandanyadav2004 <demo8788151949@gmail.com>
2025-08-14 21:02:09 +0530

    profile.jsx changes completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,23 +6,53 @@ import UserProfileInfo from "../components/UserProfileInfo";
 import PostCard from "../components/PostCard";
 import moment from "moment";
 import ProfileModal from "../components/ProfileModal";
+import { useAuth } from "@clerk/clerk-react";
+import api from "../api/axios";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const currentUser = useSelector((state) => state.user.value);
   const { profileId } = useParams();
+  const { getToken } = useAuth();
 
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState([]);
   const [activeTab, setActiveTab] = useState("posts");
   const [showEdit, setShowEdit] = useState(false);
 
-  const fetchUser = async () => {
-    setUser(dummyUserData);
-    setPosts(dummyPostsData);
+  
+
+  const fetchUser = async (profileId) => {
+    let token = await getToken();
+    try {
+      const { data } = await api.post(
+        "/api/user/profiles",
+        { profileId },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (data.success) {
+        setUser(data.profile);
+        setPosts(data.posts);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
-    fetchUser();
-  }, []);
+  if (profileId) {
+    fetchUser(profileId);
+  } else  {
+    fetchUser(currentUser._id);
+  }
+}, [profileId, currentUser?._id]);
+
+
 
   return user ? (
     <div className=" relative h-full overflow-y-scroll bg-gray-50 p-6">
@@ -42,7 +72,7 @@ const Profile = () => {
 
           <UserProfileInfo
             user={user}
-            post={posts}
+            posts={posts}
             profileId={profileId}
             setShowEdit={setShowEdit}
           />
@@ -53,7 +83,7 @@ const Profile = () => {
           <div className=" bg-white rounded-xl shadow p-1 flex max-w-md mx-auto">
             {["posts", "media", "likes"].map((tab) => (
               <button
-              onClick={() => setActiveTab(tab)}
+                onClick={() => setActiveTab(tab)}
                 key={tab}
                 className={` flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${
                   activeTab === tab
@@ -61,52 +91,50 @@ const Profile = () => {
                     : " text-gray-600 hover:text-gray-900"
                 }`}
               >
-                {
-                  tab.charAt(0).toUpperCase() + tab.slice(1)
-                }
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
               </button>
             ))}
           </div>
           {/* Posts */}
-          {
-            activeTab === 'posts' && (
-              <div className=" mt-6 flex flex-col items-center gap-6">
-                {
-                  posts.map((post) => <PostCard key={post._id} post={post}/>)
-                }
-              </div>
-            )
-          }
+          {activeTab === "posts" && (
+            <div className=" mt-6 flex flex-col items-center gap-6">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+          )}
           {/* Media */}
-          {
-            activeTab === 'media' && (
-              <div className=" flex flex-wrap mt-6 max-w-6xl ">
-                {
-                  posts.filter((post) => post.image_urls.length > 0).map((post) => (
-                    <>
-                      {
-                        post.image_urls.map((image,index) => (
-                          <Link target="_blank" to={image} key={index} className=" relative group" >
-                              <img src={image} key={index} className=" w-64 aspect-video object-cover" />
-                              <p className=" absolute bottom-0 right-0 text-xs p-1 px-3 backdrop-blur-xl text-white opacity-0 group-hover:opacity-100 transition duration-300">Posted {moment(post.createdAt).fromNow()}</p>
-                          </Link>
-                        ))
-                      }
-                    </>
-                  ))
-                }
-              </div>
-            )
-
-          }
+          {activeTab === "media" && (
+            <div className=" flex flex-wrap mt-6 max-w-6xl ">
+              {posts
+                .filter((post) => post.image_urls.length > 0)
+                .map((post) => (
+                  <>
+                    {post.image_urls.map((image, index) => (
+                      <Link
+                        target="_blank"
+                        to={image}
+                        key={index}
+                        className=" relative group"
+                      >
+                        <img
+                          src={image}
+                          key={index}
+                          className=" w-64 aspect-video object-cover"
+                        />
+                        <p className=" absolute bottom-0 right-0 text-xs p-1 px-3 backdrop-blur-xl text-white opacity-0 group-hover:opacity-100 transition duration-300">
+                          Posted {moment(post.createdAt).fromNow()}
+                        </p>
+                      </Link>
+                    ))}
+                  </>
+                ))}
+            </div>
+          )}
         </div>
       </div>
       {/* Edit Profile Modal */}
-      {
-        showEdit && 
-          <ProfileModal setShowEdit={setShowEdit} />
-        
-      }
+      {showEdit && <ProfileModal setShowEdit={setShowEdit} />}
     </div>
   ) : (
     <Loading />
